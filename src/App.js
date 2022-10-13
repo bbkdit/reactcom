dacd4880c787b8bf7f96e8c06b3b8f5ce45be000
bibek das <dasbibek189@gmail.com>
2022-10-13 17:25:31 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monster: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monster: users }));
+  }
+  render() {
+    return <div className='App'>
+      {this.state.monster.map(monster => (<h1 key={monster.id}>{monster.name}</h1>))}
+    </div>;
+  }
+}
+
+export default App;
