2cafb38e23a17f4fd084d4799e17695c39d15780
bibek das <dasbibek189@gmail.com>
2022-10-14 17:22:54 +0530

    Updates

---
+++ precache-manifest.6ba7d8ac6b8395f7e0c07afa14c5bd1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "df1a7d09408506cd1e58",
+    "url": "/reactcom/static/css/main.7c295a4e.chunk.css"
+  },
+  {
+    "revision": "df1a7d09408506cd1e58",
+    "url": "/reactcom/static/js/main.668fd339.chunk.js"
+  },
+  {
+    "revision": "1ca4057058b28ec9c77c",
+    "url": "/reactcom/static/js/runtime~main.a2ed8809.js"
+  },
+  {
+    "revision": "a994c4ebbb1dc26f096b",
+    "url": "/reactcom/static/js/2.e4e10842.chunk.js"
+  },
+  {
+    "revision": "86490d3356e6e8970c660ead75a60a72",
+    "url": "/reactcom/index.html"
+  }
+];
